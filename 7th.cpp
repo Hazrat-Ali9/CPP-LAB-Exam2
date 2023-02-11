717714d259983347837e8a8801e3bb0509e8dc57
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-02-11 17:02:12 +0600

    program added

--- 7th.cpp
+++ 7th.cpp
@@ -1,39 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void deleteValue(list<int> & p , int value){
-    auto a = p.begin();
-    int index=0;
-    while(a!=p.end()){
-        if(value==*a){
-            p.erase(a);
-            return;
+
+void deleteValue(list<int> &l, int value)
+{
+    for (list<int>::iterator i = l.begin(); i != l.end(); i++)
+    {
+        if (*i == value)
+        {
+            l.erase(i);
+            break;
         }
-        a++;
-
-
-}
-
-void printlist(list<int> &p){
-    list<int>::iterator a = p.begin();
-
-    while(a!=p.end()){
-        cout<<*a<<" ";
-        a++;
     }
-    cout<<endl;
 }
 
-int main ( ){
-    list<int> p;
-    p.push_back(7);
-    p.push_back(3);
-    p.push_back(8);
-    p.push_back(4);
-    p.push_back(5);
-    p.push_back(4);
+int main()
+{
+    list<int> l = {7, 3, 8, 4, 5, 4};
+    int x;
+    cout << "STL list containing [";
+    for (int i : l)
+        cout << i << " ";
 
+    cout << "], value: ";
+    cin >> x;
 
-    printlist(p);
-    deleteValue(p,4);
-    printlist(p);
+    deleteValue(l, x);
+    cout << "STL list containing [";
+    for (int i : l)
+        cout << i << " ";
+    cout << "]"
+         << "\n";
+
+    return 0;
 }
